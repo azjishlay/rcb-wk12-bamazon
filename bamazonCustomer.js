a2d0f56683ab211528a29284aac4d0089f552f50
Jasmine Li <li.jasmine.j@gmail.com>
2016-04-20 16:12:55 -0400

    Initial files

---
+++ bamazonCustomer.js
@@ -0,0 +1,16 @@
+// Display itmes available for sale
+// This initial display, should include the ids, names, and prices of products for sale
+
+// Prompt user
+// Ask for ID of desired product
+// Ask for quantity
+// Ask if confirm order or add more
+// Repeat until confirm
+
+// Check if product is in stock
+// If no, respond: "Insufficient quantity" and prevent the order from going through
+// If yes, show total cost of purchase
+
+// Prompt user to confirm
+// If no, reset
+// If yes, deduct from database and reset
